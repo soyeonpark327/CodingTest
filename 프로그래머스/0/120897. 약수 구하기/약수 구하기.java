504f13b59381db7d24e9e15c6c782bc4c63f2a9c
박소연 <s2608@e-mirim.hs.kr>
2026-08-07 16:07:34 +0700

    [level 0] Title: 약수 구하기, Time: 0.35 ms, Memory: 85.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] solution(int n) {
+        int count = 0;
+        
+        for(int i = 1; i < (n + 1); i++){
+            if(n % i == 0){
+                count++;
+            }
+        }
+        
+        int[] answer = new int[count];
+        int roomnum = 0;
+        
+        for(int i = 1; i < (n + 1); i++){
+            if(n % i == 0){
+                answer[roomnum] = i;
+                roomnum++;
+            }
+        }
+        
+        return answer;
+    }
+}
