780d0bcce0ec13f4a394da6ae9087fb49b826c3c
박소연 <s2608@e-mirim.hs.kr>
2026-09-07 08:09:53 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 2.92 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        
+        for(int i = 0; i < myString.length(); i++) {
+            char c = myString.charAt(i);
+            if(c < 97) {
+                c += 32;
+                answer += c;
+            } else {
+                answer += c;
+            }
+        }   
+        
+        return answer;
+    }
+}
